0c55d53f19be86bb404bd3082844f58ac7c25955
bar <barsolo10@gmail.com>
2021-12-01 13:10:41 +0200

    not finished , required commentery.

--- formValidation.js
+++ formValidation.js
@@ -2,14 +2,28 @@
 if (typeof window !== 'undefined') {
     window.onload = function() {
         //update the code that handles the form submit
+        document.querySelector("button").onclick = validate_all;
         let form = null;
         form.onsubmit =null;
     }
 }
+function validate_all(){
+    let q_text = document.getElementById("q").innerHTML;
+    let in_num = document.getElementById("num").innerHTML;
+    let q_vale = validateQ(q_text);
+    let num_vale = validateNum(in_num);
+    if(q_vale != null && num_vale != null){
+        alert(q_vale + num_vale);
+    }
+    else return;
+
+}
 //
 //return a validation message in case the input is invalid
 function validateQ(qValue) {
-    let postfix = 'the search box';
+    let postfix = 'into the search box';
+    let forbidenWords = "sex, porn, murder or violence"
+    let maxWordLength = "nine letters"
     if (hasChar(qValue) && qValue.trim().length > 1) {
         if(isNumber(qValue))    
             return '*Please enter only text ' + postfix;
@@ -25,30 +39,67 @@ function validateQ(qValue) {
 }
 //return a validation message in case the input is invalid
 function validateNum(numValue) {
+    let minResults = "1";
+    let maxResults = "25";
     if (!isInRange(numValue))    
-        return `*Please enter number ${minResults} to ${maxResults} to tell google how many search results we want`;    
+        return `*Please enter number (between) ${minResults} to ${maxResults} to tell google how many search results we want`;    
     else{
         return null;
     }
 }
 //
+//*why is this text and not a num?
 function isInRange(text){
+    if(text === "" || text < 1 || text > 25){
+        return true;
+    }
     return false;
 }
 //
-function hasForbidenWord(text) {    
+//*trying to put each word inside an array, and see if its a "bad word"
+function hasForbidenWord(text) { 
+    /*const text_array = text.split(" ");
+    const word_array = text.split("");
+    for(let x in text_array){
+        low_x = x.toLowerCase();
+        if(low_x == "sex" || low_x == "violnce" || low_x == "porn" || low_x == "murder"){
+            return true;
+        }
+    
+    }*/
+    forbiden_array = ["sex","porn","violnce","murder"];
+    for(let x in forbiden_array){
+        if(text.indexOf(x) != -1){
+            return true;
+        }
+    }
     return false;
 }
 // 
-function hasATooLongWord(text) {    
+//*trying to put each word inside an array and seeing if any of the word is too long
+function hasATooLongWord(text) {  
+    const text_array = text.split(" ");
+    for(let x in text_array){
+        if(x.length > 9){
+            return true
+        }
+    }
     return false;
 }
 //
+//*try to see if the text is blank
 function hasChar(text) {
+    if(typeof text === String){
+        return true
+    }
     return false;
 }
 //
 function isNumber(text) {
+    if(typeof text === Number){
+        return true;
+    }
+
     return false;
 }
 //Please , don't remove the following code 
